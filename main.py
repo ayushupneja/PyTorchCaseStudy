6dc5ae9ae279c2e4f858d5b7c85449c3747579f5
Ayush Upneja <upneja@bu.edu>
2019-04-26 21:08:37 -0400

    tidying up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import torch
 import matplotlib.pyplot as plt
 
-
 class MyReLU(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input):
@@ -9,55 +8,36 @@ class MyReLU(torch.autograd.Function):
         return input.clamp(min=0)
 
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx, graDoutput):
         input, = ctx.saved_tensors
-        grad_input = grad_output.clone()
-        grad_input[input < 0] = 0
-        return grad_input
-
-
+        graDinput = graDoutput.clone()
+        graDinput[input < 0] = 0
+        return graDinput
 dtype = torch.float
 device = torch.device("cpu")
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
+N, Din, H, Dout = 64, 1000, 100, 10
 
-# Create random Tensors to hold input and outputs.
-x = torch.randn(N, D_in, device=device, dtype=dtype)
-y = torch.randn(N, D_out, device=device, dtype=dtype)
+x = torch.randn(N, Din, device=device, dtype=dtype)
+y = torch.randn(N, Dout, device=device, dtype=dtype)
 
-# Create random Tensors for weights.
-w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
-w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
+weight1 = torch.randn(Din, H, device=device, dtype=dtype, requires_grad=True)
+weight2 = torch.randn(H, Dout, device=device, dtype=dtype, requires_grad=True)
 graph = list(range(0,500))
 
 learning_rate = 1e-6
 for t in range(500):
     relu = MyReLU.apply
-
-    # Forward pass: compute predicted y using operations; we compute
-    # ReLU using our custom autograd operation.
-    y_pred = relu(x.mm(w1)).mm(w2)
-
-    # Compute and print loss
+    y_pred = relu(x.mm(weight1)).mm(weight2)
     loss = (y_pred - y).pow(2).sum()
     print(t, loss.item())
     graph[t] = loss.item()
-
-
-
-    # Use autograd to compute the backward pass.
     loss.backward()
-
-    # Update weights using gradient descent
     with torch.no_grad():
-        w1 -= learning_rate * w1.grad
-        w2 -= learning_rate * w2.grad
-
-        # Manually zero the gradients after updating weights
-        w1.grad.zero_()
-        w2.grad.zero_()
+        weight1 -= learning_rate * weight1.grad
+        weight2 -= learning_rate * weight2.grad
+        weight1.grad.zero_()
+        weight2.grad.zero_()
 
 plt.plot(graph)
 plt.show()
